87b0bf7ce65e9e95ced5c2bb0f36cd23a30a36ac
or <elhararor207@gmail.com>
2023-07-09 10:41:39 +0300

    Added action's output and used on the deploy.yml

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,7 +1,7 @@
 // 1. Get some input values
 // 2. Upload the files - while doing that, the aws cli would be searching for
 //    the following credentials in the environment variables: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
-
+// 3. Output the website url
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 // const github = require('@actions/github');
@@ -20,6 +20,8 @@ function run(){
 
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`)
+    const websiteUrl = `http://${bucket}.s3-website-${region}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
 }
 
 run();
