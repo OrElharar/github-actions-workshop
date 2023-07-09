66056076cd0762b7551f966a9596de24f5e8683f
or <elhararor207@gmail.com>
2023-07-09 10:04:05 +0300

    Edited real example for deploying a static front app to aws s3

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,9 +1,25 @@
+// 1. Get some input values
+// 2. Upload the files - while doing that, the aws cli would be searching for
+//    the following credentials in the environment variables: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+
 const core = require('@actions/core');
-const github = require('@actions/github');
 const exec = require('@actions/exec');
+// const github = require('@actions/github');
+// @actions/github would be useful to get access for:
+// 1. github.context  - context of the action - providing some useful information
+//    about the event like the type of the event, the payload, the repo, etc.
+// 2. github.getOctokit - get authenticated GitHub client - a tool to interact with
+//    the GitHub API easily
 
 function run(){
     core.notice('Running deploy-s3-js action');
+
+    const bucket = core.getInput('bucket', {required: true});
+    const region = core.getInput('region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
+
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`)
 }
 
 run();
